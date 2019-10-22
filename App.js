feb83995a5655a590aa79ba5588148cbdf902b5a
bima rijaluddin <bimarijal@gmail.com>
2019-10-22 19:51:04 +0800

    Tugas 4 React JS
    Tugas dari niomic

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import MenuMakanan from './Page/MenuMakanan';
+// import ListMakanan from './ListData/ListMakanan';
+
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        
+        <MenuMakanan/>
+        {/* <ListMakanan/> */}
+      </div>
+    )
+  }
+}
+
+export default App;
